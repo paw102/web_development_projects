e5e2ea9fbad3ed2dcea9733851c0bd4b62edca9c
yongu09 <paw1023021@gmail.com>
2025-04-04 21:33:46 +0900

    feat: 20250404 delete function finished

--- combined_project/frontend/carfront/src/components/Carlist.tsx
+++ combined_project/frontend/carfront/src/components/Carlist.tsx
@@ -1,12 +1,45 @@
-import { useQuery } from "@tanstack/react-query";
-import { CarResponse } from "../types";
-import { getCars } from "../api/carapi";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getCars, deleteCar } from "../api/carapi";
+import { DataGrid, GridCellParams, GridColDef } from "@mui/x-data-grid";
 
 function Carlist() {
+  const queryClient = useQueryClient();
   const { data, error, isSuccess} = useQuery({
     queryKey: ["cars"],
     queryFn: getCars
   });
+
+  const { mutate } = useMutation(deleteCar, {
+    onSuccess : () => {
+      queryClient.invalidateQueries({queryKey: ['cars']});
+    },
+    onError : (err) => {
+      console.log(err);
+    },
+  });
+
+  const columns: GridColDef[] = [
+    {field: 'brand', headerName: 'Brand', width: 200},
+    {field: 'model', headerName: 'Model', width: 200},
+    {field: 'color', headerName: 'Color', width: 200},
+    {field: 'registrationNumber', headerName: 'Reg.nr', width: 150},
+    {field: 'modelYear', headerName: 'Model Year', width: 150},
+    {field: 'price', headerName: 'Price', width: 150},
+    {
+      field: 'edit',
+      headerName: '',
+      width: 90,
+      sortable: false,
+      filterable: false,
+      disableColumnMenu: true,
+      renderCell: (params: GridCellParams) =>
+        <button
+          onClick={() => mutate(params.row._links.car.href)}
+        >
+          삭제
+        </button>
+    }
+  ]
 
   if(!isSuccess) {
     return <span>Loading...</span>
@@ -16,21 +49,12 @@ function Carlist() {
   }
   else {
     return(
-      <table>
-        <tbody>
-          {
-            data.map((car: CarResponse)=> 
-            <tr key={car._links.self.href}>
-              <td>{car.brand}</td>
-              <td>{car.model}</td>
-              <td>{car.color}</td>
-              <td>{car.registrationNumber}</td>
-              <td>{car.modelYear}</td>
-              <td>{car.price}</td>
-            </tr>)
-          }
-        </tbody>
-      </table>
+      <DataGrid
+        rows={data}
+        columns={columns}
+        getRowId={row => row._links.self.href}
+        // disableRowSelectionOnClick = {true}
+      />
     );
   }
 }
